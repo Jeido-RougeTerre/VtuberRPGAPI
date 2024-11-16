8c912a04ebfa425840fb5a6d6dbcc64e6b4a049f
Jeido <wampawither@gmail.com>
2024-11-16 10:11:54 +0100

    Added Validators to influence DTO

--- src/main/java/com/jeido/vtuberrpgapi/dto/stat_influence/StatInfluenceDTOReceive.java
+++ src/main/java/com/jeido/vtuberrpgapi/dto/stat_influence/StatInfluenceDTOReceive.java
@@ -1,7 +1,6 @@
 package com.jeido.vtuberrpgapi.dto.stat_influence;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +13,26 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class StatInfluenceDTOReceive {
-    @NotNull
+
+    @NotNull(message = "Vtuber ID should not be Empty")
     private UUID vtuberId;
-    @NotNull
+
+    @NotNull(message = "Trigger label should not be Empty")
+    @NotEmpty(message = "Trigger label should not be Empty")
+    @NotBlank(message = "Trigger label should not be Empty")
+    @Size(min = 3, max = 25, message = "Trigger label should be between 3 and 25 characters")
+    @Pattern(regexp = "^[a-z0-9_-]{3,25}$", message = "Trigger label can have only English lower case letters, numbers, - and _ and be between 3 and 25 characters")
     private String triggerLabel;
-    @NotNull
+
+    @NotNull(message = "Stat label should not be Empty")
+    @NotEmpty(message = "Stat label should not be Empty")
+    @NotBlank(message = "Stat label should not be Empty")
+    @Size(min = 3, max = 25, message = "Stat label should be between 3 and 25 characters")
+    @Pattern(regexp = "^[a-z0-9_-]{3,25}$", message = "Stat label can have only English lower case letters, numbers, - and _ and be between 3 and 25 characters")
     private String statLabel;
+
     private String value;
+
     @Pattern(regexp = "^[+*/=-]$")
     private String operator;
 
